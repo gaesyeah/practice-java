5b7ac24f20060acda31adde30f31674c5bd08b2e
gaesyeah <gabrielgoesrios@gmail.com>
2024-01-22 16:57:36 -0300

    add: java init + Ex1

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Ex1.evenOrOdd(2);
+        Ex1.evenOrOdd(1);
+    }
+}
---
+++ src/Ex1.java
@@ -0,0 +1,9 @@
+public class Ex1 {
+  public static void evenOrOdd (int number){
+    if (number % 2 == 0){
+      System.out.println(number + " is even");
+    } else {
+      System.out.println(number + " is odd");
+    }
+  }
+}
